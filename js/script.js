7449850e16d5785852da7091492e4cc649b7be18
Olivier Scherrer <pode.fr@gmail.com>
2013-12-22 16:05:51 -0500

    add documentation

--- js/script.js
+++ js/script.js
@@ -13,17 +13,28 @@ define(function (require) {
 	return {
 
 		/**
-		 *
-		 *
+		 * Append a script to <head>
+		 * @param {Object} a script element to append
 		 */
 		append: function append(script) {
 			document.querySelector("head").appendChild(script);
 		},
 
+		/**
+		 * Remove a script from <head>
+		 * @param {Object} a script element to remove
+		 */
 		remove: function remove(script) {
 			script.parentElement.removeChild(script);
 		},
 
+		/**
+		 * Create a script element
+		 * @param {String} src the url to the resource to load
+		 * @param {Function} (optional) callback the callback to execute when the script is loaded
+		 * @param {Object} (optional) scope the scope in which to execute the callback
+		 * @returns {Script} the create script element
+		 */
 		create: function create(src, callback, scope) {
 			var script = document.createElement("script");
 			script.src = src;
