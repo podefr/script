afaf1376b7d2e7b2308ef27afe3784a71fa4dd81
Olivier Scherrer <pode.fr@gmail.com>
2013-12-23 23:34:28 -0500

    remove browser field from package

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
-all: clean script-utils.js browser
+all: clean script-utils.min.js browser
 
 clean:
 	rm -f ./script-utils.min.js
 	rm -f ./script-utils-browser.js
 
-script-utils.js:
+script-utils.min.js:
 	cat license-mini >> script-utils.min.js
 	uglifyjs js/script-utils.js >> script-utils.min.js
 
-browser:
+browser: clean script-utils.min.js
 	browserify -r ./script-utils.min.js:script-utils -o script-utils-browser.js
 
 .PHONY: all clean browser
--- script-utils-browser.js
+++ script-utils-browser.js
@@ -1,5 +1,10 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"xMiV/y":[function(require,module,exports){
-
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"iJm8Kn":[function(require,module,exports){
+/**
+ * script.js - http://podefr.github.com/script/
+ * Copyright(c) 2013 Olivier Scherrer <pode.fr@gmail.com>
+ * MIT Licensed
+ */
+"use strict";module.exports={append:function append(script){document.querySelector("head").appendChild(script)},remove:function remove(script){script.parentElement.removeChild(script)},create:function create(src,callback,scope){var script=document.createElement("script");script.src=src;script.addEventListener("load",function(){callback.apply(scope,arguments)},true);return script}};
 },{}],"script-utils":[function(require,module,exports){
-module.exports=require('xMiV/y');
+module.exports=require('iJm8Kn');
 },{}]},{},[])
